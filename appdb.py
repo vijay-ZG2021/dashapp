d159430f56aab070e8b0a6e83895e41104992728
Vijay Lakshmi <91983693+vijay-ZG2021@users.noreply.github.com>
2026-01-29 14:41:08 -0500

    Update appdb.py
    indentation corrected

--- appdb.py
+++ appdb.py
@@ -27,13 +27,13 @@ def get_last_day_of_previous_month(ASOF:date):
 def getFundOverview():
     try:
         with get_connection() as ctx:
-        with ctx.cursor() as cs:
-            cs.execute("select FUND_NAME,GAV_IN_FUND_CCY,TOTAL_LEVERAGE,REPO_LOAN_AMOUNT_FUND_CCY,MARGIN_POSTED_FUND_CCY,NET_CASH_AT_CUSTODY_FUND_CCY,CASH_RESERVES_FUND_CCY,CASH_NET_OF_RESERVES_FUND_CCY,CASH_NET_OF_RESERVES_AS_PCT_OF_GAV from ZAIS_PROD_MRT.FINCITE.FUND_OVERVIEW_REPORT" )
-            data = cs.fetchall()
-            hdrs = pd.DataFrame(cs.description)
-            df = pd.DataFrame(data)
-            df.columns= hdrs['name']
-            return df
+            with ctx.cursor() as cs:
+                cs.execute("select FUND_NAME,GAV_IN_FUND_CCY,TOTAL_LEVERAGE,REPO_LOAN_AMOUNT_FUND_CCY,MARGIN_POSTED_FUND_CCY,NET_CASH_AT_CUSTODY_FUND_CCY,CASH_RESERVES_FUND_CCY,CASH_NET_OF_RESERVES_FUND_CCY,CASH_NET_OF_RESERVES_AS_PCT_OF_GAV from ZAIS_PROD_MRT.FINCITE.FUND_OVERVIEW_REPORT" )
+                data = cs.fetchall()
+                hdrs = pd.DataFrame(cs.description)
+                df = pd.DataFrame(data)
+                df.columns= hdrs['name']
+                return df
     finally:
         cs.close()
         ctx.close()
@@ -41,13 +41,13 @@ def getFundOverview():
 def getDerivativesMetrics(date_value):
     try:
         with get_connection() as ctx:
-        with ctx.cursor() as cs:
-            cs.execute("select * from ZAIS_PROD_MRT.FINCITE.DERIVATIVES_METRICS where ASOF=%s", (date_value,))
-            data = cs.fetchall()
-            hdrs =pd.DataFrame(cs.description)
-            df = pd.DataFrame(data)
-            df.columns = hdrs['name']
-            return df
+            with ctx.cursor() as cs:
+                cs.execute("select * from ZAIS_PROD_MRT.FINCITE.DERIVATIVES_METRICS where ASOF=%s", (date_value,))
+                data = cs.fetchall()
+                hdrs =pd.DataFrame(cs.description)
+                df = pd.DataFrame(data)
+                df.columns = hdrs['name']
+                return df
     finally:
         cs.close()
         ctx.close()
@@ -55,13 +55,13 @@ def getDerivativesMetrics(date_value):
 def getPositions(date_value):
     try:
         with get_connection() as ctx:
-        with ctx.cursor() as cs:
-            cs.execute("select * ,AGGREGATE_NAME AS Fund_Name from ZAIS_PROD_MRT.FINCITE.VW_CRYSTAL_POSITIONS where as_of_date=%s", (date_value,))
-            data = cs.fetchall()
-            hdrs =pd.DataFrame(cs.description)
-            df = pd.DataFrame(data)
-            df.columns = hdrs['name']
-            return df
+            with ctx.cursor() as cs:
+                cs.execute("select * ,AGGREGATE_NAME AS Fund_Name from ZAIS_PROD_MRT.FINCITE.VW_CRYSTAL_POSITIONS where as_of_date=%s", (date_value,))
+                data = cs.fetchall()
+                hdrs =pd.DataFrame(cs.description)
+                df = pd.DataFrame(data)
+                df.columns = hdrs['name']
+                return df
     finally:
         cs.close()
         ctx.close()
@@ -71,14 +71,11 @@ def getFx(date_value):
     try:
         prev_date_value= get_last_day_of_previous_month(date_value)
         with get_connection() as ctx:
-        with ctx.cursor() as cs:
-            cs.execute("select fx.Currency_Index,fx.Mid_Price,fx.As_Of_Date from  fx_History_eod as fx where  Currency_index in ('EUR') and fx.As_Of_Date in (%s)",(date_value,))
-            data = cs.fetchall()
-            df = pd.DataFrame(data)
-            return df
+            with ctx.cursor() as cs:
+                cs.execute("select fx.Currency_Index,fx.Mid_Price,fx.As_Of_Date from  fx_History_eod as fx where  Currency_index in ('EUR') and fx.As_Of_Date in (%s)",(date_value,))
+                data = cs.fetchall()
+                df = pd.DataFrame(data)
+                return df
     finally:
         cs.close()
         ctx.close()
-
-
-
