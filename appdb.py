1f9cff2d2ceeffbce22d4446ef67b5e0d48db777
Vijay Lakshmi <91983693+vijay-ZG2021@users.noreply.github.com>
2026-02-03 19:17:02 -0500

    clean dbconnect

--- appdb.py
+++ appdb.py
@@ -8,7 +8,6 @@ from dateutil.relativedelta import relativedelta
 from sqlalchemy.dialects import registry
 
 _ENGINE: Engine | None = None
-#####Connectivity to Snowflake. Works only in Development Environment. Deployment doesn't work ######
 # appdb.py
 def get_engine() -> Engine:
     """
@@ -46,13 +45,6 @@ def getFundOverview() -> pd.DataFrame:
     with engine.connect() as conn:
         return pd.read_sql(sql, conn)
 
-        # with get_connection() as ctx:
-        #     with ctx.cursor() as cs:
-        #         cs.execute("select FUND_NAME,GAV_IN_FUND_CCY,TOTAL_LEVERAGE,REPO_LOAN_AMOUNT_FUND_CCY,MARGIN_POSTED_FUND_CCY,NET_CASH_AT_CUSTODY_FUND_CCY,CASH_RESERVES_FUND_CCY,CASH_NET_OF_RESERVES_FUND_CCY,CASH_NET_OF_RESERVES_AS_PCT_OF_GAV from ZAIS_PROD_MRT.FINCITE.FUND_OVERVIEW_REPORT" )
-        #         return pd.DataFrame(
-        #              cs.fetchall(),
-        #              columns=[c[0] for c in cs.description]
-        #         )
 
 def getDerivativesMetrics(date_value) -> pd.DataFrame:
     engine = get_engine()
@@ -61,35 +53,14 @@ def getDerivativesMetrics(date_value) -> pd.DataFrame:
         return pd.read_sql(sql, conn)
                                     
     
-    # with get_connection() as ctx:
-    #     with ctx.cursor() as cs:
-    #         cs.execute("select * from ZAIS_PROD_MRT.FINCITE.DERIVATIVES_METRICS where ASOF=%s", (date_value,))
-    #         return pd.DataFrame(
-    #                 cs.fetchall(),
-    #                 columns=[c[0] for c in cs.description]
-    #         )
- 
 def getPositions(date_value) -> pd.DataFrame:
     engine = get_engine()
     sql = text("""select * ,AGGREGATE_NAME AS FUND_NAME from ZAIS_PROD_MRT.FINCITE.VW_CRYSTAL_POSITIONS where as_of_date=:date_value""").bindparams(bindparam("date_value",date_value))
     with engine.connect() as conn:
         return pd.read_sql(sql, conn)
-    # with get_connection() as ctx:
-    #     with ctx.cursor() as cs:
-    #         cs.execute("select * ,AGGREGATE_NAME AS Fund_Name from ZAIS_PROD_MRT.FINCITE.VW_CRYSTAL_POSITIONS where as_of_date=%s", (date_value,))
-    #         return pd.DataFrame(
-    #                 cs.fetchall(),
-    #                 columns=[c[0] for c in cs.description]
-    #         )
 
 def getFx(date_value):
     engine = get_engine()
     sql = text("""select fx.Currency_Index,fx.Mid_Price,fx.As_Of_Date from  fx_History_eod as fx where  Currency_index in ('EUR') and fx.As_Of_Date in (%s)""",(date_value,))
     with engine.connect() as conn:
         return pd.read_sql(sql,conn)
-    # with get_connection() as ctx:
-    #     with ctx.cursor() as cs:
-    #         cs.execute("select fx.Currency_Index,fx.Mid_Price,fx.As_Of_Date from  fx_History_eod as fx where  Currency_index in ('EUR') and fx.As_Of_Date in (%s)",(date_value,))
-    #         return pd.DataFrame(
-    #                 cs.fetchall()                    
-    #         )
